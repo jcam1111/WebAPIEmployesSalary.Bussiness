e5ff7bf1177aac03474408bb1403141edfd2ca22
jcam1111 <44075697+jcam1111@users.noreply.github.com>
2024-10-22 23:07:46 -0500

    WebAPIEmployesSalary.Bussiness

---
+++ CalculationSalary.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPIEmployesSalary.Model;
+using WebAPIEmployesSalary.Services.Interfaces;
+
+namespace WebAPIEmployesSalary.Bussiness
+{
+
+    public class CalculationSalary
+    {
+        private readonly ICalculatorSalaryStrategy _strategy;
+
+        public CalculationSalary(ICalculatorSalaryStrategy strategy)
+        {
+            _strategy = strategy;
+        }
+
+        public decimal CalcularSalarioAnual(EmployeeAnnualSalary empleado)
+        {
+            return _strategy.OperationCalculatorAnnualSalary(empleado);
+        }
+    }
+
+}
---
+++ CalculatorAnnualSalary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPIEmployesSalary.Model;
+using WebAPIEmployesSalary.Services.Interfaces;
+
+namespace WebAPIEmployesSalary.Bussiness
+{    
+    public class CalculatorAnnualSalary : ICalculatorSalaryStrategy
+    {
+        public decimal OperationCalculatorAnnualSalary(EmployeeAnnualSalary employee)
+        {
+            employee.AnnualSalary = employee.AnnualSalary * 12; 
+            return (decimal)(employee.AnnualSalary * 12);
+        }
+    }
+}
